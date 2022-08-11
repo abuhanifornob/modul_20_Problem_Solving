afb605d2f01e566b0ee626116d4f6308346638d0
Abu Hanif Arnob <hanifcse90@gmail.com>
2022-08-11 17:09:15 +0600

    Input Validation Check

--- best_friend_check.js
+++ best_friend_check.js
@@ -1,5 +1,10 @@
 function isBestFriend(first_object, second_object) {
-    if (first_object.name === second_object.friend) {
+    // Valid Input Check
+    if (typeof(first_object) !== 'object') {
+        return "Worng Input! Please Enter Valid Input";
+    }
+    // is best Friend Check
+    else if (first_object.name === second_object.friend) {
         if (first_object.friend === second_object.name) {
             return true;
         } else {
@@ -19,5 +24,7 @@ const second_object = {
     name: "Romzan",
     friend: "Abu Hanif"
 }
+console.log(typeof(first_object));
 
-console.log(isBestFriend(first_object, second_object));
+console.log(isBestFriend(first_object,
+    second_object));
